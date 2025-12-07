1feca488a7cb62a56edf5795fc7e361f16c217aa
Reuben Bowlby <reuben@hummbl.io>
2025-12-06 23:36:45 -0500

    fix(lint): remove Icons object export to satisfy react-refresh rule

--- apps/web/src/components/ui/icons/index.tsx
+++ apps/web/src/components/ui/icons/index.tsx
@@ -2,11 +2,5 @@ import { Spinner } from './Spinner';
 import { Google } from './Google';
 import { Github } from './Github';
 
-export const Icons = {
-  spinner: Spinner,
-  google: Google,
-  github: Github,
-};
-
 // Export individual components
 export { Spinner, Google, Github };
