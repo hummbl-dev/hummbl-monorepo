09e450bed41ae41ebb49d09c9157a786db480f35
Reuben Bowlby <reuben@hummbl.io>
2025-12-06 23:40:39 -0500

    fix(build): correct api export in lib/index.ts

--- apps/web/src/lib/index.ts
+++ apps/web/src/lib/index.ts
@@ -1,5 +1,5 @@
 // Re-export the API instance
-export { default as api } from './api';
+export { api } from './api';
 
 // Export any other utilities or types from the lib directory
 // export * from './other-utilities';
